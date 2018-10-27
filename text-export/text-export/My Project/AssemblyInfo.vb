200d6bad37e893f961b2f303e1264c297996f67e
关倩倩 <amethyst.asuka@gcmodeller.org>
2018-10-28 01:30:34 +0800

    move directory

---
+++ text-export/text-export/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("text-export")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("text-export")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("678674f4-ac62-4ee5-bd28-8468b98b8608")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ text-export/text-export/Program.vb
@@ -0,0 +1,18 @@
+﻿Imports Microsoft.VisualBasic.Language.UnixBash
+Imports Microsoft.VisualBasic.Serialization.JSON
+
+Module Program
+
+    Sub Main()
+        For Each file As String In ls - l - r - "*.json" <= "../../json/text/"
+            file.LoadJSON(Of text) _
+                .text _
+                .Tsv($"../../text/{file.BaseName}.xls")
+        Next
+    End Sub
+End Module
+
+Public Class text
+    Public Property status As String
+    Public Property text As Dictionary(Of String, String)
+End Class
