4418f4580972f6c508d55c84356c06f7ca5d86ff
关倩倩 <amethyst.asuka@gcmodeller.org>
2018-10-28 04:31:40 +0800

    delay js exec

--- styles/hide.js
+++ styles/hide.js
@@ -1,2 +1,4 @@
-document.getElementsByTagName("header")[0].style.display = "none";
-document.getElementById("sidebar").style.display = "none";
+window.setTimeout(function() {
+    document.getElementsByTagName("header")[0].style.display = "none";
+    document.getElementById("sidebar").style.display = "none";
+}, 300);
